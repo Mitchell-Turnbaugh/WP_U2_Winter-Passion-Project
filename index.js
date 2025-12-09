80b415923dbd644df8fdb25269852495cf2731c3
Mitchell Turnbaugh <MitchellTurnbaughCProg@gmail.com>
2025-12-09 08:40:50 -0500

    Added hyper

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ function baseLog(base,argument){
 }
 function factorial(argument){
     if(argument < 0){
-        return "value must not be negative";
+        return NaN;
     }
     if(!argument.isInteger){
-        return "vaule must be an integer";
+        return NaN;
     }
     result = 1
     for(let i = 2; i++; i <= argument){
@@ -30,4 +30,31 @@ function product(numbers){
         answer *= i;
     }
     return answer;
+}
+function hyper(a,level,n){
+    if(level >= 4){
+        if(n.isInteger){
+            for(let i = 0; i < n; i++){
+                return hyper(a,level-1,n);
+            }
+        }else{
+            return NaN;
+        }
+    }else if(level === 3){
+        return a ** n;
+    }else if(level === 2){
+        return a * n;
+    }else if(level === 1){
+        return a + n;
+    }else if(level === 0){
+        return a + 1;
+    }else if(level === -1){
+        return a - n;
+    }else if(level === -2){
+        return a / n;
+    }else if(level === -3){
+        return root(a,n);
+    }else{
+        return NaN;
+    }
 }
