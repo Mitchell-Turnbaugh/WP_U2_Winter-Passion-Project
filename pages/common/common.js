ed826ca15bb9241c8c605fefc9be9c02ecec3429
Mitchell Turnbaugh <MitchellTurnbaughCProg@gmail.com>
2025-12-17 09:33:58 -0500

    Same as last

---
+++ pages/common/common.js
@@ -0,0 +1,65 @@
+function answer(){
+    const equation = document.getElementById("equation");
+    if(equation.textContent[equation.textContent.length - 1] === "."){
+        press("0(" + Number(sessionStorage.getItem("answer")) + ")")
+        return;
+    }
+    for(const i of "0123456789."){
+        if(equation.textContent[equation.textContent.length - 1] === i){
+            press("(" + Number(sessionStorage.getItem("answer")) + ")");
+            return;
+        }
+    }
+    press(Number(sessionStorage.getItem("answer")));
+}
+function backspace(){
+    const equation = document.getElementById("equation");
+    equation.textContent = equation.textContent.slice(0,-1);
+}
+function empty(){
+    const equation = document.getElementById("equation");
+    const answer = document.getElementById("answer");
+    equation.textContent = "";
+    answer.textContent = "";
+}
+function press(pressed){
+    const equation = document.getElementById("equation");
+    const answer = document.getElementById("answer");
+    if(answer.textContent !== ""){
+        empty();
+    }
+    equation.textContent += pressed;
+}
+function solve(){
+    let equation = document.getElementById("equation").textContent;
+    const answer = document.getElementById("answer");
+    equation = equation.replaceAll("÷","/");
+    parenthesis = [];
+    for(let i = equation.indexOf("("); i < equation.length && i !== undefined;){
+        for(const j of "0123456789"){
+            if(equation[i - 1] == j){
+                console.log("Test")
+                parenthesis.push(i);
+                i = equation.indexOf("(",i + 1);
+                break;
+            }
+        }
+    }
+    for(const i of parenthesis){
+        first = equation.slice(0,i);
+        end = equation.slice(i);
+        equation = first + "*" + end;
+    }
+    while(equation.includes("--")){
+        equation = equation.replaceAll("--","+");
+    }
+    while(equation.includes("++")){
+        equation = equation.replaceAll("++","+");
+    }
+    try{
+        answer.textContent = eval(equation);
+        sessionStorage.setItem("answer",answer.textContent);
+    }catch{
+        answer.textContent = "Math Error";
+    }
+}
